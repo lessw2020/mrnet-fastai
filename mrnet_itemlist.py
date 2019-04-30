230cb3aabd724573a9e58ed8df828fe4d2e0191a
Nick Switanek <nswitanek@gmail.com>
2019-04-30 12:15:03 -0500

    add __init__ and .get methods to MRNetCaseList

--- mrnet_itemlist.py
+++ mrnet_itemlist.py
@@ -5,7 +5,7 @@ from fastai.vision import *
 class MRNetCase(ItemBase):
 
   # ItemBase.data   # this needs to be developed in parallel with the ItemList's .get method
-  # imagine that .get will return a tuple of three np.arrays, an erray for each plane
+  # imagine that .get will return a list of three np.arrays, an array for each plane
     def __init__(self, axial, coronal, sagittal):
         self.axial,self.coronal,self.sagittal = axial,coronal,sagittal
         self.obj = (axial,coronal,sagittal)
@@ -18,16 +18,39 @@ class MRNetCase(ItemBase):
         pass        
   # apply_tfms (optional)
 
+
+
 # ItemList subclass
+class MRNetCaseList(ItemList):
+
   # class variables
     # _bunch
     # _processor
     # _label_cls
 
   # __init__ arguments
+  # items for this subclass will likely be a list/iterator of Case strings
+  # rather than filenames, since each case has 3 filenames, one for each plane
+    def __init__(self, items, **kwargs):
+        super().__init__(items, **kwargs)
 
   # core methods
     # get
+    def get(self, i):
+        # i indexes self.items, which is a list of Case strings
+        case = super().get(i)
+        imagearrays = []
+        for plane in ('axial','coronal','sagittal'):
+            # self.path is available from kwargs of ItemList superclass
+            fn  = self.path/plane/(case + '.npy')
+            res = self.open(fn)
+            imagearrays.append(res)
+        assert len(imagearrays) == 3
+        return MRNetCase(*imagearrays)
+
+    # since subclassing ItemList rather than ImageList, need an open method
+    def open(self, fn): return np.load(fn)
+
     # reconstruct
     # analyze_pred
 
