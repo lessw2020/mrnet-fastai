bfc8637ec384c91ca88b27e3de2e7567ce81112b
Nick Switanek <nswitanek@gmail.com>
2019-04-30 11:03:19 -0500

    __init__ method for MRNetCase, a custom ItemBase class; hard-coding the .data attribute to be middle three slices of sagittal series, for now

--- mrnet_itemlist.py
+++ mrnet_itemlist.py
@@ -1,8 +1,21 @@
 import numpy as np
+from fastai.vision import *
 
 # ItemBase subclass
-  # ItemBase.data
+class MRNetCase(ItemBase):
+
+  # ItemBase.data   # this needs to be developed in parallel with the ItemList's .get method
+  # imagine that .get will return a tuple of three np.arrays, an erray for each plane
+    def __init__(self, axial, coronal, sagittal):
+        self.axial,self.coronal,self.sagittal = axial,coronal,sagittal
+        self.obj = (axial,coronal,sagittal)
+        # for .data, initially hard-code taking the middle three slices of the sagittal series
+        # middle three grayscale slices, instead of repeating same middle slice 3x
+        smid = sagittal.shape[0]//2
+        self.data = sagittal[smid-1:smid+2,:,:]
   # __str__ representation
+    def __str__(self):
+        pass        
   # apply_tfms (optional)
 
 # ItemList subclass
