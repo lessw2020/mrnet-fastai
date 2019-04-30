904ab2e7f8035a3b06e89db1c60b6d129b5d1ea3
Nick Switanek <nswitanek@gmail.com>
2019-04-30 10:36:56 -0500

    basic todo scaffold for custom ItemBase and ItemList subclasses

---
+++ mrnet_itemlist.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# ItemBase subclass
+  # ItemBase.data
+  # __str__ representation
+  # apply_tfms (optional)
+
+# ItemList subclass
+  # class variables
+    # _bunch
+    # _processor
+    # _label_cls
+
+  # __init__ arguments
+
+  # core methods
+    # get
+    # reconstruct
+    # analyze_pred
+
+  # advanced show methods
+    # show_xys
+    # show_xyzs
