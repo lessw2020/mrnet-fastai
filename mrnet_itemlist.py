a04f45e30fd9928e58617f5e446d979dad5c50ff
Nick Switanek <nswitanek@gmail.com>
2019-05-02 14:39:25 -0500

    adjusted get method for CaseList; added from_folder method for constructing CaseList from MRNet data folder

--- mrnet_itemlist.py
+++ mrnet_itemlist.py
@@ -17,21 +17,34 @@ class MRNetCase(ItemBase):
     def __repr__(self): 
         return f'''
         {self.__class__.__name__} 
-        .obj attribute is tuple(axial, coronal, sagittal): 
+        `obj` attribute is tuple(axial, coronal, sagittal): 
         {list(e.shape for e in self.obj)}
             
-        .data attribute is three middle slices of sagittal
+        `data` attribute is three middle slices of sagittal
         {self.data.shape}
         {self.data}
         '''
 
   # apply_tfms (optional)
 
+# DataBunch subclass
+class MRNetCaseDataBunch(DataBunch):
+    "DataBunch for MRNet knee scan data."
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def from_df(cls, path:PathOrStr, df:pd.DataFrame, folder:PathOrStr=None, label_delim:str=None, valid_pct:float=0.2,
+                fn_col:IntsOrStrs=0, label_col:IntsOrStrs=1, suffix:str='', **kwargs:Any)->'ImageDataBunch':
+        "Create DataBunch from a `DataFrame` `df`."
+        src = (MRNetCaseList.from_df(df, path=path, folder=folder, suffix=suffix, cols=fn_col)
+                .split_by_rand_pct(valid_pct)
+                .label_from_df(label_delim=label_delim, cols=label_col))
+        return cls.create_from_ll(src, **kwargs)
 
 
 # ItemList subclass
 class MRNetCaseList(ItemList):
-
   # class variables
     _bunch = MRNetCaseDataBunch
     # _processor
@@ -46,12 +59,16 @@ class MRNetCaseList(ItemList):
   # core methods
     # get
     def get(self, i):
-        # i indexes self.items, which is a list of Case strings
-        case = super().get(i)
+        # i indexes self.items, which is an array of filepaths(filenames)
+        fn = super().get(i)
+        # .stem returns the Case number, as a string
+        case = fn.stem
+        # also collect whether the case returned with get(i) is in train or valid folder
+        tv = 'train' if 'train' in fn.parts else 'valid'
         imagearrays = []
         for plane in ('axial','coronal','sagittal'):
             # self.path is available from kwargs of ItemList superclass
-            fn  = self.path/plane/(case + '.npy')
+            fn  = self.path/tv/plane/(case + '.npy')
             res = self.open(fn)
             imagearrays.append(res)
         assert len(imagearrays) == 3
@@ -61,26 +78,25 @@ class MRNetCaseList(ItemList):
     def open(self, fn): return np.load(fn)
 
     # TODO: reconstruct
+    def reconstruct(self, t, x):
+        # I think t is the tensor corresponding to the .data attribute of MRNetCase
+        # and x is the tuple of tensors corresponding to the .obj attribute of MRNetCase
+        # not sure if x is an MRNetCase or if x is MRNetCase.obj
+        # the result of reconstruct should be to 
+        # "return the same kind of object as .get returns"
+        # which is a MRNetCase 
+        # and to build that, the entire tuple of tensors x is required
+        return MRNetCase(x) # or maybe MRNetCase(x.obj)
 
     # TODO: analyze_pred
 
-    # TODO: from_df
     @classmethod
-    def from_df():
-        pass
+    def from_folder(cls, path:PathOrStr='.', extensions:Collection[str]=['.npy'], **kwargs)->'MRNetCaseList':
+        "Get the filenames for all MRNet cases, assuming directory structure unchanged from MRNet data download"
+        return super().from_folder(path=path, extensions=extensions, **kwargs)
 
-  # advanced show methods
+
+    # advanced show methods
     # show_xys
     # show_xyzs
 
-class MRNetCaseDataBunch(DataBunch):
-    "DataBunch for MRNet knee scan data."
-
-    @classmethod
-    def from_df(cls, path:PathOrStr, df:pd.DataFrame, folder:PathOrStr=None, label_delim:str=None, valid_pct:float=0.2,
-                fn_col:IntsOrStrs=0, label_col:IntsOrStrs=1, suffix:str='', **kwargs:Any)->'ImageDataBunch':
-        "Create DataBunch from a `DataFrame` `df`."
-        src = (MRNetCaseList.from_df(df, path=path, folder=folder, suffix=suffix, cols=fn_col)
-                .split_by_rand_pct(valid_pct)
-                .label_from_df(label_delim=label_delim, cols=label_col))
-        return cls.create_from_ll(src, **kwargs)
